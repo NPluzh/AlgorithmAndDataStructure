aee562975bfbdb14eeaf657f9d18da2b1ed9daf6
whittema <shamarsen@mail.ru>
2022-10-18 13:02:25 +0300

    task 1 done

---
+++ src/main/java/homework5/Main.java
@@ -0,0 +1,9 @@
+package homework5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DegreeCalculator.degree(2,5));
+        System.out.println(DegreeCalculator.degree(3,2));
+        System.out.println(DegreeCalculator.degree(2,10));
+    }
+}
